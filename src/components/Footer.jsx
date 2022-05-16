61837ee2eb2db0dd7f4b0b7cbc8647fbabaf612b
Reiyn <98770205+iamreiyn@users.noreply.github.com>
2022-05-16 16:34:26 +0530

    Initial release

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+function Footer() {
+
+    return (
+        <div>
+            <footer className="page-footer font-small blue">
+
+            <div className="card">
+  <ul className="list-group list-group-flush my-5">
+    <center><a href="https://github.com/renisal">Lua Patterns - An open source and personal project</a></center>
+  </ul>
+</div>
+
+</footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/components/Menu.jsx
@@ -0,0 +1,29 @@
+function Menu() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <img className="mx-3" src="https://i.postimg.cc/5ynzMQD0/rens.png" alt="Ren's Lua Patterns"/>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item active">
+        <a className="nav-link" href="/">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="https://github.com/renisal">Creator</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">Source</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="https://github.com/renisal?tab=repositories">More Apps</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+    );
+}
+
+export default Menu;
