1166841abcbbf5815fee3bfd3a74e90dd9f7d14b
Reiyn <98770205+iamreiyn@users.noreply.github.com>
2022-05-16 17:21:00 +0530

    minor improvements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,8 @@ function Footer() {
 
             <div className="card">
   <ul className="list-group list-group-flush my-5">
-    <center><a href="https://github.com/renisal">Lua Patterns - An open source and personal project</a></center>
+    <center><a href="https://github.com/renisal">Lua Patterns: match, debug & build online</a></center>
+    <center><strong>An open source and personal project</strong></center>
   </ul>
 </div>
 
